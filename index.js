be86740f404c464126a9b21a60d54f16006afdea
Somesh <somesh_nj2_076@masai.school>
2021-06-24 18:36:51 +0530

    Completed movie-1 assignment

---
+++ index.js
@@ -0,0 +1,69 @@
+let cont = document.getElementById("data_cont");
+async function getMovieData() {
+  let movie_title = document.getElementById("movie_title").value;
+  try {
+    let res = await fetch(
+      `http://www.omdbapi.com/?t=${movie_title}&apikey=4bfcc3ab`
+    );
+    let data = await res.json();
+    showData(data);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function getMovie(event) {
+  if (event.key === "Enter") {
+    getMovieData();
+  }
+}
+
+function showData(data) {
+  let div = document.createElement("div");
+  let img = document.createElement("img");
+  img.src = data.Poster;
+
+  let title = document.createElement("p");
+  title.innerHTML = "Title: " + data.Title;
+
+  let release = document.createElement("p");
+  release.innerHTML = "Release Date: " + data.Released;
+
+  let rating = document.createElement("p");
+  rating.innerHTML = "Rating: " + data.imdbRating;
+
+  let actors = document.createElement("p");
+  actors.innerHTML = "Actors: " + data.Actors;
+  div.append(img, title, release, actors, rating);
+  cont.innerHTML = "";
+  cont.appendChild(div);
+}
+
+/*
+
+Actors: "Shah Rukh Khan, Kajol, Sheetal Menon"
+Awards: "23 wins & 26 nominations"
+BoxOffice: "$4,018,771"
+Country: "India, United States, United Arab Emirates, Hong Kong"
+DVD: "25 Nov 2015"
+Director: "Karan Johar"
+Genre: "Drama"
+Language: "Hindi, English"
+Metascore: "50"
+Plot: "An Indian Muslim man with Asperger's syndrome takes a challenge to speak to the President of the United States seriously and embarks on a cross-country journey."
+Poster: "https://m.media-amazon.com/images/M/MV5BMTUyMTA4NDYzMV5BMl5BanBnXkFtZTcwMjk5MzcxMw@@._V1_SX300.jpg"
+Production: "Dharma Productions, Red Chillies Entertainment, Fox Searchlight"
+Rated: "PG-13"
+Ratings: (3) [{…}, {…}, {…}]
+Released: "12 Feb 2010"
+Response: "True"
+Runtime: "165 min"
+Title: "My Name Is Khan"
+Type: "movie"
+Website: "N/A"
+Writer: "Shibani Bathija, Niranjan Iyengar"
+Year: "2010"
+imdbID: "tt1188996"
+imdbRating: "8.0"
+imdbVotes: "101,013"
+*/
